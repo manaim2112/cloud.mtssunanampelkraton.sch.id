fee959936182b88f8281410336bf2118e1b550b3
manaim2112 <m.a.naim2112@gmail.com>
2023-05-10 15:10:47 +0700

    remove

--- server.go
+++ server.go
@@ -1,16 +1,6 @@
 package main
 
 import (
-	"embed"
-	"fmt"
-	"io"
-	"io/fs"
-	"log"
-	"mime"
-	"os"
-	"path/filepath"
-	"strings"
-
 	"cloud.mtssunanampelkraton.sch.id/config"
 	"cloud.mtssunanampelkraton.sch.id/routes"
 	"github.com/goccy/go-json"
@@ -50,7 +40,6 @@ func main() {
 		})
 
 	} else {
-		app.Get("/", handleStatic)
 
 		routes.Db(db)
 		routes.RouteInstall(app)
@@ -65,48 +54,4 @@ func main() {
 	}
 
 	app.Listen(":5001")
-}
-
-var ui embed.FS
-
-func handleStatic(c *fiber.Ctx) error {
-	uiFS, err := fs.Sub(ui, "./public")
-	if err != nil {
-		log.Fatal("failed to get ui fs", err)
-	}
-
-	path := filepath.Clean(c.Path())
-	if path == "/" { // Add other paths that you route on the UI side here
-		path = "index.html"
-	}
-	path = strings.TrimPrefix(path, "/")
-
-	file, err := uiFS.Open(path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			log.Println("file", path, "not found:", err)
-			return fiber.NewError(fiber.StatusNotFound, "file not found")
-		}
-		log.Println("file", path, "cannot be read:", err)
-		return fiber.NewError(fiber.StatusInternalServerError, "Error")
-	}
-	defer file.Close()
-
-	contentType := mime.TypeByExtension(filepath.Ext(path))
-	c.Set("Content-Type", contentType)
-	if strings.HasPrefix(path, "/") {
-		c.Set("Cache-Control", "public, max-age=31536000")
-	}
-	stat, err := file.Stat()
-	if err == nil && stat.Size() > 0 {
-		c.Set("Content-Length", fmt.Sprintf("%d", stat.Size()))
-	}
-
-	n, err := io.Copy(c.Response().BodyWriter(), file)
-	if err != nil {
-		return err
-	}
-
-	log.Println("file", path, "copied", n, "bytes")
-	return nil
 }
